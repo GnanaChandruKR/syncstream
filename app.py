e9543b021b3dd371ea1fc21edb823e749cd3422a
Chandru Ash <94100183+GnanaChandruKR@users.noreply.github.com>
2026-09-17 13:20:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 from ytmusicapi import YTMusic
